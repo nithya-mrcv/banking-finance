8425ce1e4d308b9f1304731dde691f8b45eaa0b7
nithya-mrcv <nithyasreemrcv@gmail.com>
2024-10-23 11:00:00 +0530

    Update creds.tf

--- terraform/creds.tf
+++ terraform/creds.tf
@@ -1,5 +1,3 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.aws_access_key   # Use a variable for access key
-  secret_key = var.aws_secret_key   # Use a variable for secret key
 }
