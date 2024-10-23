c7e44749347a84511f6cfafae2fbc7231ce91868
nithya-mrcv <nithyasreemrcv@gmail.com>
2024-10-23 18:12:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,22 @@
 resource "aws_instance" "jenkins-terraform" {
   ami             = "ami-0e86e20dae9224db8"
-  key_name        = "Nithya"
-  security_groups = ["proj-bank"] 
+  key_name        = "jenkin"
+  security_groups = ["sg-0b16f3692a548fa2d"]  # Updated to use the new security group ID
   instance_type   = "t2.micro"
   count           = 1
   tags = {
-    Name = "jenkins-terraform"
+    Name = "production"
   }
 }
+
+resource "aws_instance" "jenkins-terraform" {
+  ami             = "ami-0e86e20dae9224db8"
+  key_name        = "jenkin"
+  security_groups = ["sg-0b16f3692a548fa2d"]  # Updated to use the new security group ID
+  instance_type   = "t2.micro"
+  count           = 1
+  tags = {
+    Name = "prometheusgrafana"
+  }
+}
+
