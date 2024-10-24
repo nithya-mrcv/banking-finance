6df05fa6f92f5b4b11523b63dd2d88969512ccfd
nithya-mrcv <nithyasreemrcv@gmail.com>
2024-10-24 10:20:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,21 @@
-resource "aws_instance" "production" {
+resource "aws_instance" "productionserver" {
   ami                    = "ami-0e86e20dae9224db8"
   key_name               = "jenkin"
   vpc_security_group_ids = ["sg-0b16f3692a548fa2d"]  # Directly use the security group ID
   instance_type          = "t2.micro"
   count                  = 1
   tags = {
-    Name = "production"
+    Name = "productionserver"
   }
 }
 
-resource "aws_instance" "grafana" {
+resource "aws_instance" "grafanaserver" {
   ami                    = "ami-0e86e20dae9224db8"
   key_name               = "jenkin"
   vpc_security_group_ids = ["sg-0b16f3692a548fa2d"]  # Directly use the security group ID
   instance_type          = "t2.micro"
   count                  = 1
   tags = {
-    Name = "prometheus"
+    Name = "prometheusserver"
   }
 }
